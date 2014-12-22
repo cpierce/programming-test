3ee3fa63434a0f29ee9b653c7c13cc6fb9211667
Chris Pierce <cpierce@csdurant.com>
2014-12-22 21:21:02 +0000

    footer.php edited to remove extra </div> tag.

--- View/Layouts/footer.php
+++ View/Layouts/footer.php
@@ -1,9 +1,8 @@
-			</div>
 		</div>
-		<div id="footer">
-			<div class="container text-center">
-				<p>Written by Chris Pierce</p>
-			</div>
+	</div>
+	<div id="footer">
+		<div class="container text-center">
+			<p>Written by Chris Pierce</p>
 		</div>
 	</div>
 </body>
